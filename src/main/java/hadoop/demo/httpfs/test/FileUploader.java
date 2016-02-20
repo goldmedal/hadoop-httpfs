c5cc74c1deb97c113ea96e781619835beec93444
Jax <liugs963@gmail.cm>
2016-02-20 17:01:04 +0800

    add list file and read file api

--- src/main/java/hadoop/demo/httpfs/test/FileUploader.java
+++ src/main/java/hadoop/demo/httpfs/test/FileUploader.java
@@ -16,7 +16,8 @@ public class FileUploader
     	
     	String outputRoot = "/demo/";
     	
-        uploader.uploadFile(inputRoot + inputFileName, outputRoot + inputFileName);
+        // uploader.uploadFile(inputRoot + inputFileName, outputRoot + inputFileName);
+    	uploader.listFile(outputRoot);
     }
     
    public void uploadFile(String inputPath, String outputPath) throws IOException
@@ -49,5 +50,91 @@ public class FileUploader
     	System.out.println(responseCode); // Should be 200    	
     	
     }
+   
+   public void readFile(String filePath) throws IOException{
+	   
+		String url = "http://140.116.86.246:924/webhdfs/v1" + filePath + "?user.name=hduser&op=OPEN";
+	   	System.out.println(url);
+	   	String charset = "UTF-8";
+	   	String CRLF = "\r\n"; // Line separator required by multipart/form-data.
+	
+	   	HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+	   	
+	   	try
+	   	{
+	   		connection.setDoOutput(true);
+	       	connection.setRequestMethod("GET");
+	       	connection.setRequestProperty("Content-Type", "application/octet-stream");
+	       	connection.setDoOutput(true);
+	       	
+	       	connection.connect();
+	       	InputStream is = connection.getInputStream();
+	       	BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+	       	
+	       	StringBuilder response = new StringBuilder();
+	       	String line;
+	       	
+	       	while((line = rd.readLine()) != null){
+	       		
+	       		response.append(line);
+	       		response.append(CRLF);
+	       		
+	       	}
+	       	
+	       	System.out.println(response.toString());
+  	    
+	   	} catch(Exception e) {
+	   		e.printStackTrace();
+	   	}
+	
+	   	// Request is lazily fired whenever you need to obtain information about response.
+	   	int responseCode = ((HttpURLConnection) connection).getResponseCode();
+	   	System.out.println(responseCode); // Should be 200   
+       
+   }
+   
+   public void listFile(String filePath) throws IOException{
+	   
+		String url = "http://140.116.86.246:924/webhdfs/v1" + filePath + "?user.name=hduser&op=LISTSTATUS";
+	   	System.out.println(url);
+	   	String charset = "UTF-8";
+	   	String CRLF = "\r\n"; // Line separator required by multipart/form-data.
+	
+	   	HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+	   	
+	   	try
+	   	{
+	   		connection.setDoOutput(true);
+	       	connection.setRequestMethod("GET");
+	       	connection.setRequestProperty("Content-Type", "application/octet-stream");
+	       	connection.setDoOutput(true);
+	       	
+	       	connection.connect();
+	       	InputStream is = connection.getInputStream();
+	       	BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+	       	
+	       	StringBuilder response = new StringBuilder();
+	       	String line;
+	       	
+	       	while((line = rd.readLine()) != null){
+	       		
+	       		response.append(line);
+	       		response.append(CRLF);
+	       		
+	       	}
+	       	
+	       	System.out.println(response.toString());
+ 	    
+	   	} catch(Exception e) {
+	   		e.printStackTrace();
+	   	}
+	
+	   	// Request is lazily fired whenever you need to obtain information about response.
+	   	int responseCode = ((HttpURLConnection) connection).getResponseCode();
+	   	System.out.println(responseCode); // Should be 200   
+      
+  }
+   
+   
     
 }
